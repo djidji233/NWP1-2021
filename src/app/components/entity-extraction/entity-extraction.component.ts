1a0b3080599badbe7b0cda69cb536d79c13500cf
Djordje Zivanovic <djolezile@gmail.com>
2021-11-05 00:24:41 +0100

    still CORS problems. last commit.

--- src/app/components/entity-extraction/entity-extraction.component.ts
+++ src/app/components/entity-extraction/entity-extraction.component.ts
@@ -26,7 +26,13 @@ export class EntityExtractionComponent implements OnInit {
 
   search(text: string, min_confidence: number, includeParams: string) {
     if(this.include){
-
+      let res = includeParams.split(',')
+      let imageStr = res[0].length>1 ? res[0] : '';
+      let abstractStr = res[1].length>1 ? res[1] : '';
+      let categoriesStr = res[2].length>1 ? res[2] : '';
+      this.entityExtractionService.search(text,min_confidence,imageStr,abstractStr,categoriesStr).subscribe((res)=>{
+        console.log(res)
+      })
     } else {
       this.entityExtractionService.search(text,min_confidence,'','','').subscribe((res)=>{
           console.log(res)
